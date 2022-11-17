423d11f2b124caee8a3bba6fe9fd966e04830d5f
test1 <laphael0508@gmail.com>
2022-11-18 03:37:22 +0900

    sixteenth commit

--- js/login.js
+++ js/login.js
@@ -1,12 +1,13 @@
 const id = document.getElementById('id')
-const password = document.getElementById('password')
+const passwd = document.getElementById('passwd')
 const login = document.getElementById('login')
 let errStack = 0;
 
 login.addEventListener('click', () => {
-    if (id.value == 'acid') {
-        if (password.value == '0000') {
+    if (id.value == 'admin') {
+        if (passwd.value == 'password') {
             alert('로그인 되었습니다!')
+            location.href="index.html";
         }
         else {
             alert('아이디와 비밀번호를 다시 한 번 확인해주세요!')
